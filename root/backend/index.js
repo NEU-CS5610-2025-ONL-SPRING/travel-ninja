4f75843608ac94ee925ed6682ae8465150a2adb7
shreyassahu <shreyassahu2208@gmail.com>
2025-03-26 19:25:02 -0400

    Added auth functionality

--- root/backend/index.js
+++ root/backend/index.js
@@ -1,62 +1,104 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import express from "express";
-import { PrismaClient } from "@prisma/client";
+import pkg from "@prisma/client";
+import morgan from "morgan";
+import cors from "cors";
+import jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
+import bcrypt from "bcrypt";
 
 const app = express();
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cookieParser());
 
-
+const { PrismaClient } = pkg;
 const prisma = new PrismaClient();
 
-app.use(express.json());
+// Middleware to verify JWT token sent by the client
+function requireAuth(req, res, next) {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
 
-app.get("/user", async (req, res) => {
-  const users = await prisma.user.findMany({});
-  res.json(users);
+    // attaching the user id to the request object, this will make it available in the endpoints that use this middleware
+    req.userId = payload.userId;
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+}
+
+// this is a public endpoint because it doesn't have the requireAuth middleware
+app.get("/ping", (req, res) => {
+  res.send("pong");
 });
 
-app.post("/user", async (req, res) => {
-  const { email, name } = req.body;
-  const user = await prisma.user.create({
-    data: {
-      email,
-      name,
-    },
+app.post("/register", async (req, res) => {
+  const { email, password, name } = req.body;
+  const existingUser = await prisma.user.findUnique({ where: { email } });
+  if (existingUser) {
+    return res.status(400).json({ error: "User already exists" });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = await prisma.user.create({
+    data: { email, password: hashedPassword, name },
+    select: { id: true, email: true, name: true },
+  });
+
+  const payload = { userId: newUser.id };
+  const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "15m" });
+  res.cookie("token", token, { httpOnly: true, maxAge: 15 * 60 * 1000 });
+
+  res.json(newUser);
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await prisma.user.findUnique({ where: { email } });
+  if (!user) {
+    return res.status(401).json({ error: "Invalid credentials" });
+  }
+  // password: 123456
+  // user.password: $2b$10$naV1eAwirV13nyBYVS96W..52QzN8U/UQ7mmi/IEEVJDtCAdDmOl2
+  const validPassword = await bcrypt.compare(password, user.password);
+  if (!validPassword) {
+    return res.status(401).json({ error: "Invalid credentials" });
+  }
+
+  const payload = { userId: user.id };
+  const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "15m" });
+  res.cookie("token", token, { httpOnly: true, maxAge: 15 * 60 * 1000 });
+
+  // ensure that the password is not sent to the client
+  const userData = {
+    id: user.id,
+    email: user.email,
+    name: user.name,
+  };
+
+  res.json(userData);
+});
+
+app.post("/logout", async (req, res) => {
+  res.clearCookie("token");
+  res.json({ message: "Logged out" });
+});
+
+// requireAuth middleware will validate the access token sent by the client and will return the user information within req.auth
+app.get("/me", requireAuth, async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: { id: req.userId },
+    select: { id: true, email: true, name: true },
   });
   res.json(user);
-});
-
-app.post("/todo", async (req, res) => {
-  const { title, authorEmail } = req.body;
-  const todoItem = await prisma.todoItem.create({
-    data: {
-      title,
-      author: { connect: { email: authorEmail } },
-    },
-  });
-  res.json(todoItem);
-});
-
-app.get("/todos/:email", async (req, res) => {
-  const email = req.params.email;
-  const todoItems = await prisma.todoItem.findMany({
-    where: {
-      author: {
-        email: email,
-      },
-    },
-  });
-  res.json(todoItems);
-});
-
-app.get("/todos/:userid", async (req, res) => {
-  const userid = req.params.userid;
-  const todoItems = await prisma.todoItem.findMany({
-    where: {
-      author: {
-        id: userid,
-      },
-    },
-  });
-  res.json(todoItems);
 });
 
 app.listen(8000, () => {
